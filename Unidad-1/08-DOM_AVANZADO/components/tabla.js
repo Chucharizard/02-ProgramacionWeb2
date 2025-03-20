5f8d7ae20beaa299fa75d07eefe8946c1f0f7fa3
Chucharizard <willyguevara048@gmail.com>
2025-03-20 10:23:42 -0400

    Añadir funcionalidad para gestionar tareas en la tabla: incluir botones de completar y eliminar

---
+++ Unidad-1/08-DOM_AVANZADO/components/tabla.js
@@ -0,0 +1,42 @@
+const tabla = (() => {
+    const cuerpoTabla = document.getElementById('taskTable').getElementsByTagName('tbody')[0]; //recupero el cuerpo de la tabla
+    //recupero el cuerpo de la tabla    
+
+    const addTask = (task) => {
+        const nuevaFila = cuerpoTabla.insertRow(); //inserto una fila en la tabla
+        nuevaFila.insertCell(0).textContent = task.task; //inserto una celda en la fila
+        nuevaFila.insertCell(1).textContent = task.description; 
+        nuevaFila.insertCell(2).textContent = task.date;
+        nuevaFila.insertCell(3).textContent = task.priority;
+
+        const accionCell = nuevaFila.insertCell(4); 
+        const accions = document.createElement('div'); //creo un div
+        accions.className= 'actions'; //le asigno una clase
+
+        //crear boton
+        const completeButton = document.createElement('button'); 
+        completeButton.textContent = 'completar';
+        completeButton.className = 'view';
+        completeButton.addEventListener('click',()=>{
+            nuevaFila.classList.toggle('completed');
+
+        });
+
+        accions.appendChild(completeButton); //agrego el boton al div
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'eliminar';
+        deleteButton.className = 'delete';
+        deleteButton.addEventListener('click',()=>{
+            cuerpoTabla.deleteRow(nuevaFila.rowIndex);
+
+
+        });
+
+        accions.appendChild(deleteButton); //agrego el boton al div
+
+        accionCell.appendChild(accions); //agrego el div a la celda
+
+    };
+
+    })
