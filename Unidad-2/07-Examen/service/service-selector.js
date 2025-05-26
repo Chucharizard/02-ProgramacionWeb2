410011ecd71e02dcd5356c0422c80e516e04ab13
Chucho <willyguevara048@gmail.com>
2025-05-26 18:51:45 -0400

    update

--- Unidad-2/07-Examen/service/service-selector.js
+++ Unidad-2/07-Examen/service/service-selector.js
@@ -1,25 +1,25 @@
-// Configuración de backend
+// Confi del backend
 const backendType = localStorage.getItem('backendType') || 'supabase'; // 'supabase', 'php', 'json', 'sqlserver'
 
-// Imports para Supabase
+// aca impotamos para Supabase
 import { peliculaService as peliculaServiceSupa } from './pelicula-service.js';
 import { funcionService as funcionServiceSupa } from './funcion-service.js';
 import { clienteService as clienteServiceSupa } from './cliente-service.js';
 import { boletoService as boletoServiceSupa } from './boleto-service.js';
 
-// Imports para PHP
+// aca impotamos paraPHP
 import { peliculaService as peliculaServicePhp } from './pelicula-service-php.js';
 import { funcionService as funcionServicePhp } from './funcion-service-php.js';
 import { clienteService as clienteServicePhp } from './cliente-service-php.js';
 import { boletoService as boletoServicePhp } from './boleto-service-php.js';
 
-// Imports para JSON
+// aca impotamos para JSON
 import { peliculaService as peliculaServiceJson } from './pelicula-service-json.js';
 import { funcionService as funcionServiceJson } from './funcion-service-json.js';
 import { clienteService as clienteServiceJson } from './cliente-service-json.js';
 import { boletoService as boletoServiceJson } from './boleto-service-json.js';
 
-// Imports para SQL Server 
+// aca impotamos para Server 
 import { peliculaService as peliculaServiceSqlServer } from './pelicula-service-sqlserver.js';
 import { funcionService as funcionServiceSqlServer } from './funcion-service-sqlserver.js';
 import { clienteService as clienteServiceSqlServer } from './cliente-service-sqlserver.js';
